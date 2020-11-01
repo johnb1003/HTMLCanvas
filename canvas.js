5c753c63087889749ce901018c5335c01070c4d0
John Burns <johnb1003@gmail.com>
2020-10-31 20:34:40 -0400

    Added canvas functionality and fixed Snake class bugs

--- canvas.js
+++ canvas.js
@@ -1,27 +1,14 @@
 var canvas;
 var context;
 var difficulty = 'difficulty-0';
-var speeds = [600, 400, 200];
+var speeds = [300, 200, 100];
 var game;
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function canvasInit() {
-    canvas = document.getElementById('myCanvas'),
-    context = canvas.getContext('2d');
-}
-
-function drawRectangle() {
-    context.fillStyle = 'rgba(200, 40, 40)';
-    context.fillRect(50, 50, 20, 20);
-}
-
 $(document).ready( () => {
-    canvasInit();
-    game = new SnakeGame(speed);
-
     $('.difficulty-button').click( (e) => {
         if(!$(e.target).hasClass('active-difficulty')) {
             $(`#${difficulty}`).removeClass('active-difficulty');
@@ -43,16 +30,16 @@ $(document).ready( () => {
     // Swipe touchscreen input
     $(document).swipe({
         swipeLeft:function(event, distance, duration, fingerCount, fingerData, currentDirection) {
-            game.direction = 'left';
+            game.changeDirection('left');
         },
         swipeUp:function(event, distance, duration, fingerCount, fingerData, currentDirection) {
-            game.direction = 'up';
+            game.changeDirection('up');
         },
         swipeRight:function(event, distance, duration, fingerCount, fingerData, currentDirection) {
-            game.direction = 'right';
+            game.changeDirection('right');
         },
         swipeDown:function(event, distance, duration, fingerCount, fingerData, currentDirection) {
-            game.direction = 'up';
+            game.changeDirection('down');
         }
     });
 
@@ -61,22 +48,22 @@ $(document).ready( () => {
         switch(e.keyCode) {
             case 37: 
             // Left
-            game.direction = 'left';
+            game.changeDirection('left');
             break;
 
             case 38: 
             // Up
-            game.direction = 'up';
+            game.changeDirection('up');
             break;
 
             case 39: 
             // Right
-            game.direction = 'right';
+            game.changeDirection('right');
             break;
 
             case 40: 
             // Down
-            game.direction = 'down';
+            game.changeDirection('down');
             break;
 
             default:
@@ -94,14 +81,17 @@ $(document).ready( () => {
 class SnakeGame {
     constructor(snakeSpeed) {
         this.speed = snakeSpeed;
-        this.dimensions = 10;
+        this.dimensions = 20;
         this.model = [];
         this.openSpaces = [];
         this.snake = new LinkedList();
         this.itemCoordinates = [];
         this.isLive = false;
+        this.lastDirection = 'right';
         this.direction = 'right';
         this.score = 0;
+        this.context = document.getElementById('myCanvas').getContext('2d');
+        this.canvasUnit = document.getElementById('myCanvas').width / this.dimensions;
 
         for(let i=0; i<this.dimensions; i++) {
             this.model.push(new Array(this.dimensions).fill(0));
@@ -109,53 +99,82 @@ class SnakeGame {
 
         for(let i=0; i<this.dimensions; i++) {
             for(let j=0; j<this.dimensions; j++) {
-                this.openSpaces.add([i, j]);
+                this.openSpaces.push([i, j]);
             }
         }
     }
 
     init() {
-        // CLEAR CANVAS
+        this.clearCanvas();
 
-        this.addSnake(4, 0);
-        this.addSnake(4, 1);
-        this.addSnake(4, 2);
-        this.addSnake(4, 3);
+        //console.log(`Open Spaces: ${this.openSpaces.length}`);
+        
+        this.addSnake(Math.floor(this.dimensions / 2), 0);
+        this.addSnake(Math.floor(this.dimensions / 2), 1);
+        this.addSnake(Math.floor(this.dimensions / 2), 2);
+        this.addSnake(Math.floor(this.dimensions / 2), 3);
         this.addRandomItem();
-
-        this.start();
+        this.play();
     }
 
-    start() {
+    async play() {
         this.isLive = true;
+        
+        while(this.isLive) {
+            //console.log('Move');
+            await this.move();
+            await sleep(this.speed);
+        }
+
+        console.log('Game Over');
     }
 
     gameOver() {
         this.isLive = false;
     }
 
+    clearCanvas() {
+        this.context.fillStyle = 'rgb(0, 0, 0)';
+        this.context.fillRect(0, 0, this.canvasUnit * this.dimensions, this.canvasUnit * this.dimensions);
+    }
+
+    changeDirection(dir) {
+        if (this.lastDirection === 'left' || this.lastDirection === 'right') {
+            if(dir === 'up' || dir === 'down') {
+                this.direction = dir;
+            }
+        }
+        else if (this.lastDirection === 'up' || this.lastDirection === 'down') {
+            if(dir === 'left' || dir === 'right') {
+                this.direction = dir;
+            }
+        }
+    }
+
     addSnake(x, y) {
         this.model[x][y] = 1;
-        this.snake.add(new Node(x, y));
+        this.snake.add(x, y);
         this.removeOpenSpace(x, y);
+        this.drawSnake(x, y);
     }
 
     removeSnake() {
-        let node = this.snake.remove(x, y);
+        let node = this.snake.remove();
         if(node != null) {
             this.model[node.x][node.y] = 0;
             this.addOpenSpace(node.x, node.y);
+            this.undrawSnake(node.x, node.y)
         }
     }
 
     addOpenSpace(x, y) {
-        this.openSpaces.add([x,y]);
+        this.openSpaces.push([x,y]);
     }
 
     removeOpenSpace(x, y) {
         for(let i=0; i<this.openSpaces.length; i++) {
             if(this.openSpaces[i][0] === x && this.openSpaces[i][1] === y) {
-                this.openSpaces = this.openSpaces.splice(i, 1);
+                this.openSpaces.splice(i, 1);
                 return;
             }
         }
@@ -164,49 +183,88 @@ class SnakeGame {
     addRandomItem() {
         let randomIndex = Math.floor(Math.random() * this.openSpaces.length);
         this.itemCoordinates = this.openSpaces[randomIndex];
-        this.model[itemCoords[0]][this.itemCoordinates[1]] = 2;
+        this.model[this.itemCoordinates[0]][this.itemCoordinates[1]] = 2;
         this.removeOpenSpace(this.itemCoordinates[0], this.itemCoordinates[1]);
+        this.drawItem(this.itemCoordinates[0], this.itemCoordinates[1]);
     }
 
-    move() {
+    removeRandomItem() {
+        this.model[this.itemCoordinates[0]][this.itemCoordinates[1]] = 0;
+        this.addOpenSpace(this.itemCoordinates[0], this.itemCoordinates[1]);
+        this.undrawItem(this.itemCoordinates[0], this.itemCoordinates[1]);
+    }
+
+    async move() {
         let nextCoords = [];
         if(this.direction === 'up') {
+            this.lastDirection = 'up';
             nextCoords = [this.snake.head.x - 1, this.snake.head.y];
         }
         else if(this.direction === 'left') {
+            this.lastDirection = 'left';
             nextCoords = [this.snake.head.x, this.snake.head.y-1];
         }
         else if(this.direction === 'right') {
+            this.lastDirection = 'right';
             nextCoords = [this.snake.head.x, this.snake.head.y+1];
         }
         else if(this.direction === 'down') {
+            this.lastDirection = 'down';
             nextCoords = [this.snake.head.x+1, this.snake.head.y];
         }
 
+        //console.log(this.direction);
+
         // If next move is within bounds
         if(nextCoords[0] < this.dimensions && nextCoords[0] >= 0 && nextCoords[1] < this.dimensions && nextCoords[1] >= 0) {
+            //console.log('Move is inbounds');
             // If hit own body
             if(this.snake.contains(nextCoords[0], nextCoords[1])) {
                 // end game
-                this.gameOver();
+                console.log('Move: Hit tail');
+                await this.gameOver();
                 return;
             }
             // If found item
-            else if(this.itemCoordinates[0] === nextCoords[0] && this.itemCoordinates[0] === nextCoords[1]) {
+            else if(this.itemCoordinates[0] === nextCoords[0] && this.itemCoordinates[1] === nextCoords[1]) {
                 // add to head but dont remove tail
-                this.addSnake(nextCoords[0], nextCoords[1]);
-                this.addRandomItem();
+                console.log('Move: Found item');
+                await this.removeRandomItem(nextCoords[0], nextCoords[1]);
+                await this.addSnake(nextCoords[0], nextCoords[1]);
+                await this.addRandomItem();
+                this.score++;
+
             }
             // Move to open space
             else {
-                this.addSnake(nextCoords[0], nextCoords[1]);
-                this.removeSnake();
+                //console.log('Move: Open space');
+                await this.addSnake(nextCoords[0], nextCoords[1]);
+                await this.removeSnake();
             }
         }
         // Hit wall
         else {
-            this.gameOver();
+            console.log('Move: Hit wall');
+            await this.gameOver();
         }
+    }
+
+    drawSnake(x, y) {
+        this.context.fillStyle = 'rgb(250, 250, 250)';
+        this.context.fillRect(y * this.canvasUnit, x * this.canvasUnit, this.canvasUnit, this.canvasUnit);
+    }
+
+    undrawSnake(x, y) {
+        this.context.clearRect(y * this.canvasUnit, x * this.canvasUnit, this.canvasUnit, this.canvasUnit);
+    }
+
+    drawItem(x, y) {
+        this.context.fillStyle = 'rgb(177, 0, 0)';
+        this.context.fillRect(y * this.canvasUnit, x * this.canvasUnit, this.canvasUnit, this.canvasUnit);
+    }
+
+    undrawItem(x, y) {
+        this.context.clearRect(y * this.canvasUnit, x * this.canvasUnit, this.canvasUnit, this.canvasUnit);
     }
 }
 
@@ -251,11 +309,12 @@ class LinkedList {
     }
 
     contains(x, y) {
-        let currNode = head;
+        let currNode = this.head;
         while(currNode != null) {
             if(currNode.x == x && currNode.y == y) {
                 return true;
             }
+            currNode = currNode.next;
         }
         return false;
     }
